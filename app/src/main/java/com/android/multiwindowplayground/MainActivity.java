c895c69d7197c0a88cf21c50f8f0ac343dc31db4
Your Name <you@example.com>
2023-12-29 18:09:42 +0800

    add freeform launch ,thank for Taskbar share this

--- app/src/main/java/com/android/multiwindowplayground/MainActivity.java
+++ app/src/main/java/com/android/multiwindowplayground/MainActivity.java
@@ -16,6 +16,15 @@
 
 package com.android.multiwindowplayground;
 
+import android.app.ActivityOptions;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import com.android.multiwindowplayground.activities.AdjacentActivity;
 import com.android.multiwindowplayground.activities.BasicActivity;
 import com.android.multiwindowplayground.activities.CustomConfigurationChangeActivity;
@@ -23,13 +32,6 @@ import com.android.multiwindowplayground.activities.LaunchBoundsActivity;
 import com.android.multiwindowplayground.activities.LoggingActivity;
 import com.android.multiwindowplayground.activities.MinimumSizeActivity;
 import com.android.multiwindowplayground.activities.UnresizableActivity;
-
-import android.app.ActivityOptions;
-import android.content.Intent;
-import android.graphics.Rect;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 public class MainActivity extends LoggingActivity {
 
@@ -103,8 +105,8 @@ public class MainActivity extends LoggingActivity {
 
         // Start an Activity with the default options in the 'singleTask' launch mode as defined in
         // the AndroidManifest.xml.
-        startActivity(new Intent(this, BasicActivity.class));
-
+       // startActivity(new Intent(this, BasicActivity.class));
+        FreeformLauncher.test(this,new Intent(this, BasicActivity.class));
     }
 
     public void onStartCustomConfigurationActivity(View view) {
@@ -115,3 +117,63 @@ public class MainActivity extends LoggingActivity {
 
     }
 }
+
+
+
+
+ class FreeformLauncher {
+
+    // Function to launch an app in freeform window mode using an Intent
+    public static void launchAppInFreeformWindow(Context context, Intent intent, int left, int top, int width, int height) {
+        // Set the bounds for the freeform window
+        Rect bounds = new Rect(left, top, left + width, top + height);
+
+        // Get the freeform window mode id based on the Android version
+        int freeformWindowModeId = getFreeformWindowModeId();
+
+        // Create an ActivityOptions instance
+        ActivityOptions options = ActivityOptions.makeBasic();
+
+        // Set the freeform window mode and bounds using reflection
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                // Set the windowing mode to freeform
+                setActivityOptionMethod(options, "setLaunchWindowingMode", freeformWindowModeId);
+
+                // Set the window bounds for the freeform window
+                options.setLaunchBounds(bounds);
+            }
+        } catch (Exception e) {
+            // Handle exceptions if reflection fails
+            e.printStackTrace();
+        }
+
+        // Start the activity with the options bundle
+        context.startActivity(intent, options.toBundle());
+    }
+
+    private static int getFreeformWindowModeId() {
+        // Define the constants for windowing modes (taken from hidden API values)
+        final int WINDOWING_MODE_FREEFORM = 5;
+
+        // Return the freeform window mode id
+        return WINDOWING_MODE_FREEFORM;
+    }
+
+    private static void setActivityOptionMethod(ActivityOptions options, String methodName, int value) throws Exception {
+        // Use reflection to call a method on ActivityOptions to set the windowing mode
+        java.lang.reflect.Method method = ActivityOptions.class.getMethod(methodName, int.class);
+        method.invoke(options, value);
+    }
+
+    public static void test(Context context, Intent intent){
+        //Intent intent = new Intent(context, YourActivity.class);
+        int left = 50; // x position
+        int top = 200; // y position
+        int width = 500; // width of the freeform window
+        int height = 300; // height of the freeform window
+        FreeformLauncher.launchAppInFreeformWindow(context, intent, left, top, width, height);
+    }
+}
+
+
